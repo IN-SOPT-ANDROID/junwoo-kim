c88b0b5a8cd204010c3086e1fe7056f14b8695f5
IslandofDream <rhffldkt0517@naver.com>
2022-10-10 00:03:14 +0900

    [Feat] Fragment transaction , BottomNavigation Set - Fragment transaction 세팅 - BottomNavigation 세팅

--- app/src/main/java/org/sopt/sample/HomeActivity.kt
+++ app/src/main/java/org/sopt/sample/HomeActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import org.sopt.sample.model.UserData
 import org.sopt.sample.databinding.ActivityHomeBinding
+import org.sopt.sample.home.GalleryFragment
+import org.sopt.sample.home.HomeFragment
+import org.sopt.sample.home.SearchFragment
 
 class HomeActivity : AppCompatActivity() {
 
@@ -14,6 +17,40 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
         getResult()
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.container_home)
+        if (currentFragment == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.container_home, HomeFragment.newInstance())
+                .commit()
+        }
+        binding.navBottomHome.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container_home, HomeFragment.newInstance())
+                        .commit()
+                    return@setOnItemSelectedListener true
+
+                }
+                R.id.nav_gallery -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container_home, GalleryFragment.newInstance())
+                        .commit()
+                    return@setOnItemSelectedListener true
+
+                }
+                R.id.nav_search -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container_home, SearchFragment.newInstance())
+                        .commit()
+                    return@setOnItemSelectedListener true
+
+                }
+            }
+            return@setOnItemSelectedListener false
+        }
+
+
     }
 
     private fun getResult() { // LoginActivity가 보낸 intent 수신
--- app/src/main/java/org/sopt/sample/home/HomeFragment.kt
+++ app/src/main/java/org/sopt/sample/home/HomeFragment.kt
@@ -10,16 +10,9 @@ import org.sopt.sample.databinding.FragmentHomeBinding
 import org.sopt.sample.home.adapter.SampleAdapter
 import org.sopt.sample.model.RepoData
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -27,7 +20,7 @@ class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding: FragmentHomeBinding
-        get() = requireNotNull(binding)
+        get() = requireNotNull(_binding) {"${this.id} binding error"}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,23 +49,8 @@ class HomeFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() = HomeFragment()
     }
 
     override fun onDestroyView() {
