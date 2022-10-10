69434295c790c1b4bc45f69a7a41b2758461cdbc
IslandofDream <rhffldkt0517@naver.com>
2022-10-10 13:20:54 +0900

    [Feat] RecyclerView Set, ViewHolder Merge - 리싸이클러뷰 더미데이터 세팅 - ViewHolder 어댑터 내부에 통합

--- app/src/main/java/org/sopt/sample/home/HomeFragment.kt
+++ app/src/main/java/org/sopt/sample/home/HomeFragment.kt
@@ -10,13 +10,8 @@ import org.sopt.sample.databinding.FragmentHomeBinding
 import org.sopt.sample.home.adapter.SampleAdapter
 import org.sopt.sample.model.RepoData
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     private var _binding: FragmentHomeBinding? = null
     private val binding: FragmentHomeBinding
@@ -24,10 +19,6 @@ class HomeFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -45,7 +36,7 @@ class HomeFragment : Fragment() {
             )
         )
         binding.rcvHome.adapter = adapter
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return binding.root
     }
 
     companion object {
--- app/src/main/java/org/sopt/sample/home/adapter/SampleAdapter.kt
+++ app/src/main/java/org/sopt/sample/home/adapter/SampleAdapter.kt
@@ -1,5 +1,6 @@
 package org.sopt.sample.home.adapter
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -7,14 +8,28 @@ import androidx.recyclerview.widget.RecyclerView
 import org.sopt.sample.databinding.ItemHomeBinding
 import org.sopt.sample.model.RepoData
 
-class SampleAdapter(context:Context):RecyclerView.Adapter<SampleViewHolder>() {
+class SampleAdapter(context: Context):RecyclerView.Adapter<SampleAdapter.SampleViewHolder>() {
+    //class SampleAdapter():RecyclerView.Adapter<SampleViewHolder>() {
+    //view에서 context를 얻어올수 있는데 왜 context를 파라미터를 통해서 받아 오네요
+
     private val inflater by lazy { LayoutInflater.from(context) }
     private var repoList : List<RepoData> = listOf()
+
+
+    class SampleViewHolder(private val binding: ItemHomeBinding):RecyclerView.ViewHolder(binding.root)
+    {
+        fun onBind(data : RepoData){
+            //binding.imageView = data.img
+            binding.tv1.text = data.name
+            binding.tv2.text = data.des
+        }
+    }
 
     override fun getItemCount() = repoList.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SampleViewHolder {
-        val binding = ItemHomeBinding.inflate(inflater, parent,false)
+        //val binding = ItemHomeBinding.inflate(LayoutInflater.from(parent.context), parent,false)
+        val binding = ItemHomeBinding.inflate(inflater ,parent,false)
         return SampleViewHolder(binding)
     }
 
@@ -22,8 +37,10 @@ class SampleAdapter(context:Context):RecyclerView.Adapter<SampleViewHolder>() {
         holder.onBind(repoList[position])
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     fun setDataList(newdata : List<RepoData>){
         repoList = newdata.toList()
+        notifyDataSetChanged()
     }
 
 }
