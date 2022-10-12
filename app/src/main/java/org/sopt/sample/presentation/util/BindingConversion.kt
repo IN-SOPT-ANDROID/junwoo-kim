1b99267fcdedd595fd6d81ef15b3d50df38e9983
IslandofDream <rhffldkt0517@naver.com>
2022-10-12 23:51:19 +0900

    [Feat] BindAdapter apply - touch에 따른 font weight 변경 - touch에 따른 Backgroudn cololr 변경

--- app/src/main/java/org/sopt/sample/presentation/util/BindingConversion.kt
+++ app/src/main/java/org/sopt/sample/presentation/util/BindingConversion.kt
@@ -1,21 +1,30 @@
 package org.sopt.sample.presentation.util
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.res.ResourcesCompat
 import androidx.databinding.BindingAdapter
 import org.sopt.sample.R
 
 @SuppressLint("UseCompatLoadingForDrawables")
-@BindingAdapter("app:touch_gitItem")
-fun touch(view: TextView, selected:Boolean){
+@BindingAdapter("app:textView_touch_gitItem")
+fun textviewtouch(view: TextView, selected:Boolean){
     if (selected) {
         view.typeface = ResourcesCompat.getFont(view.context, R.font.nanumgothicextrabold)
-        view.setCompoundDrawablesWithIntrinsicBounds(
-            null, null, view.context.getDrawable(R.drawable.ic_check), null
-        )
     } else {
         view.typeface = ResourcesCompat.getFont(view.context, R.font.nanumgothicbold)
-        view.setCompoundDrawables(null, null, null, null)
     }
 }
+
+@SuppressLint("ResourceAsColor")
+@BindingAdapter("app:layout_touch_gitItem")
+fun layouttouch(view: ConstraintLayout, selected:Boolean){
+    if (selected) {
+        view.setBackgroundColor(Color.parseColor("#808080"))
+    } else {
+        view.setBackgroundColor(Color.parseColor("#FFFFFF"))
+    }
+}
+
