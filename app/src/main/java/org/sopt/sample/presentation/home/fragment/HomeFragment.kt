7f2c7e6441d2cc75b150080ea76ffaf89388c324
IslandofDream <rhffldkt0517@naver.com>
2022-10-13 02:29:42 +0900

    [Feat] Add Floating Button - 아이템을 삭제할 Floating Button 추가

--- app/src/main/java/org/sopt/sample/presentation/home/fragment/HomeFragment.kt
+++ app/src/main/java/org/sopt/sample/presentation/home/fragment/HomeFragment.kt
@@ -2,55 +2,242 @@ package org.sopt.sample.presentation.home.fragment
 
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.selection.SelectionPredicates
+import androidx.recyclerview.selection.SelectionTracker
+import androidx.recyclerview.selection.StableIdKeyProvider
+import androidx.recyclerview.selection.StorageStrategy
 import androidx.recyclerview.widget.RecyclerView
 import org.sopt.sample.R
 import org.sopt.sample.databinding.FragmentHomeBinding
 import org.sopt.sample.presentation.base.BindingFragment
 import org.sopt.sample.presentation.home.adapter.GitAdapter
+import org.sopt.sample.presentation.home.adapter.GitDetailsLookUp
 import org.sopt.sample.presentation.home.model.GitData
 
 class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
     companion object {
 
-        private lateinit var recyclerView : RecyclerView
+        private lateinit var recyclerView: RecyclerView
 
         @JvmStatic
         fun newInstance() = HomeFragment()
 
-        fun setScroll(){
+        fun setScroll() {
             recyclerView.smoothScrollToPosition(0)
         }
     }
 
+    private val gitList = listOf(
+        GitData(1, "", "OB", "", 0),
+        GitData(2, "", "권용민", "OB", 1),
+        GitData(3, "", "YB", "", 0),
+        GitData(4, "", "김준우", "YB", 1),
+        GitData(5, "", "김지영", "YB", 1),
+        GitData(6, "", "김우남", "YB", 1),
+        GitData(7, "", "김준우", "YB", 1),
+        GitData(8, "", "김준우", "YB", 1),
+        GitData(9, "", "김준우", "YB", 1),
+        GitData(10, "", "김준우", "YB", 1),
+        GitData(11, "", "김준우", "YB", 1),
+        GitData(12, "", "김준우", "YB", 1),
+        GitData(13, "", "김준우", "YB", 1),
+        GitData(14, "", "김준우", "YB", 1),
+        GitData(15, "", "김준우", "YB", 1),
+        GitData(16, "", "김준우", "YB", 1),
+        GitData(17, "", "김준우", "YB", 1),
+        GitData(18, "", "김준우", "YB", 1),
+        GitData(19, "", "김준우", "YB", 1),
+        GitData(20, "", "김준우", "YB", 1),
+        GitData(1, "", "OB", "", 0),
+        GitData(2, "", "권용민", "OB", 1),
+        GitData(3, "", "YB", "", 0),
+        GitData(4, "", "김준우", "YB", 1),
+        GitData(5, "", "김지영", "YB", 1),
+        GitData(6, "", "김우남", "YB", 1),
+        GitData(7, "", "김준우", "YB", 1),
+        GitData(8, "", "김준우", "YB", 1),
+        GitData(9, "", "김준우", "YB", 1),
+        GitData(10, "", "김준우", "YB", 1),
+        GitData(11, "", "김준우", "YB", 1),
+        GitData(12, "", "김준우", "YB", 1),
+        GitData(13, "", "김준우", "YB", 1),
+        GitData(14, "", "김준우", "YB", 1),
+        GitData(15, "", "김준우", "YB", 1),
+        GitData(16, "", "김준우", "YB", 1),
+        GitData(17, "", "김준우", "YB", 1),
+        GitData(18, "", "김준우", "YB", 1),
+        GitData(19, "", "김준우", "YB", 1),
+        GitData(20, "", "김준우", "YB", 1),
+        GitData(1, "", "OB", "", 0),
+        GitData(2, "", "권용민", "OB", 1),
+        GitData(3, "", "YB", "", 0),
+        GitData(4, "", "김준우", "YB", 1),
+        GitData(5, "", "김지영", "YB", 1),
+        GitData(6, "", "김우남", "YB", 1),
+        GitData(7, "", "김준우", "YB", 1),
+        GitData(8, "", "김준우", "YB", 1),
+        GitData(9, "", "김준우", "YB", 1),
+        GitData(10, "", "김준우", "YB", 1),
+        GitData(11, "", "김준우", "YB", 1),
+        GitData(12, "", "김준우", "YB", 1),
+        GitData(13, "", "김준우", "YB", 1),
+        GitData(14, "", "김준우", "YB", 1),
+        GitData(15, "", "김준우", "YB", 1),
+        GitData(16, "", "김준우", "YB", 1),
+        GitData(17, "", "김준우", "YB", 1),
+        GitData(18, "", "김준우", "YB", 1),
+        GitData(19, "", "김준우", "YB", 1),
+        GitData(20, "", "김준우", "YB", 1),
+        GitData(1, "", "OB", "", 0),
+        GitData(2, "", "권용민", "OB", 1),
+        GitData(3, "", "YB", "", 0),
+        GitData(4, "", "김준우", "YB", 1),
+        GitData(5, "", "김지영", "YB", 1),
+        GitData(6, "", "김우남", "YB", 1),
+        GitData(7, "", "김준우", "YB", 1),
+        GitData(8, "", "김준우", "YB", 1),
+        GitData(9, "", "김준우", "YB", 1),
+        GitData(10, "", "김준우", "YB", 1),
+        GitData(11, "", "김준우", "YB", 1),
+        GitData(12, "", "김준우", "YB", 1),
+        GitData(13, "", "김준우", "YB", 1),
+        GitData(14, "", "김준우", "YB", 1),
+        GitData(15, "", "김준우", "YB", 1),
+        GitData(16, "", "김준우", "YB", 1),
+        GitData(17, "", "김준우", "YB", 1),
+        GitData(18, "", "김준우", "YB", 1),
+        GitData(19, "", "김준우", "YB", 1),
+        GitData(20, "", "김준우", "YB", 1),
+        GitData(1, "", "OB", "", 0),
+        GitData(2, "", "권용민", "OB", 1),
+        GitData(3, "", "YB", "", 0),
+        GitData(4, "", "김준우", "YB", 1),
+        GitData(5, "", "김지영", "YB", 1),
+        GitData(6, "", "김우남", "YB", 1),
+        GitData(7, "", "김준우", "YB", 1),
+        GitData(8, "", "김준우", "YB", 1),
+        GitData(9, "", "김준우", "YB", 1),
+        GitData(10, "", "김준우", "YB", 1),
+        GitData(11, "", "김준우", "YB", 1),
+        GitData(12, "", "김준우", "YB", 1),
+        GitData(13, "", "김준우", "YB", 1),
+        GitData(14, "", "김준우", "YB", 1),
+        GitData(15, "", "김준우", "YB", 1),
+        GitData(16, "", "김준우", "YB", 1),
+        GitData(17, "", "김준우", "YB", 1),
+        GitData(18, "", "김준우", "YB", 1),
+        GitData(19, "", "김준우", "YB", 1),
+        GitData(20, "", "김준우", "YB", 1),
+        GitData(1, "", "OB", "", 0),
+        GitData(2, "", "권용민", "OB", 1),
+        GitData(3, "", "YB", "", 0),
+        GitData(4, "", "김준우", "YB", 1),
+        GitData(5, "", "김지영", "YB", 1),
+        GitData(6, "", "김우남", "YB", 1),
+        GitData(7, "", "김준우", "YB", 1),
+        GitData(8, "", "김준우", "YB", 1),
+        GitData(9, "", "김준우", "YB", 1),
+        GitData(10, "", "김준우", "YB", 1),
+        GitData(11, "", "김준우", "YB", 1),
+        GitData(12, "", "김준우", "YB", 1),
+        GitData(13, "", "김준우", "YB", 1),
+        GitData(14, "", "김준우", "YB", 1),
+        GitData(15, "", "김준우", "YB", 1),
+        GitData(16, "", "김준우", "YB", 1),
+        GitData(17, "", "김준우", "YB", 1),
+        GitData(18, "", "김준우", "YB", 1),
+        GitData(19, "", "김준우", "YB", 1),
+        GitData(20, "", "김준우", "YB", 1),
+        GitData(1, "", "OB", "", 0),
+        GitData(2, "", "권용민", "OB", 1),
+        GitData(3, "", "YB", "", 0),
+        GitData(4, "", "김준우", "YB", 1),
+        GitData(5, "", "김지영", "YB", 1),
+        GitData(6, "", "김우남", "YB", 1),
+        GitData(7, "", "김준우", "YB", 1),
+        GitData(8, "", "김준우", "YB", 1),
+        GitData(9, "", "김준우", "YB", 1),
+        GitData(10, "", "김준우", "YB", 1),
+        GitData(11, "", "김준우", "YB", 1),
+        GitData(12, "", "김준우", "YB", 1),
+        GitData(13, "", "김준우", "YB", 1),
+        GitData(14, "", "김준우", "YB", 1),
+        GitData(15, "", "김준우", "YB", 1),
+        GitData(16, "", "김준우", "YB", 1),
+        GitData(17, "", "김준우", "YB", 1),
+        GitData(18, "", "김준우", "YB", 1),
+        GitData(19, "", "김준우", "YB", 1),
+        GitData(20, "", "김준우", "YB", 1),
+        GitData(1, "", "OB", "", 0),
+        GitData(2, "", "권용민", "OB", 1),
+        GitData(3, "", "YB", "", 0),
+        GitData(4, "", "김준우", "YB", 1),
+        GitData(5, "", "김지영", "YB", 1),
+        GitData(6, "", "김우남", "YB", 1),
+        GitData(7, "", "김준우", "YB", 1),
+        GitData(8, "", "김준우", "YB", 1),
+        GitData(9, "", "김준우", "YB", 1),
+        GitData(10, "", "김준우", "YB", 1),
+        GitData(11, "", "김준우", "YB", 1),
+        GitData(12, "", "김준우", "YB", 1),
+        GitData(13, "", "김준우", "YB", 1),
+        GitData(14, "", "김준우", "YB", 1),
+        GitData(15, "", "김준우", "YB", 1),
+        GitData(16, "", "김준우", "YB", 1),
+        GitData(17, "", "김준우", "YB", 1),
+        GitData(18, "", "김준우", "YB", 1),
+        GitData(19, "", "김준우", "YB", 1),
+        GitData(20, "", "김준우", "YB", 1),
+    )
+    private lateinit var gitadapter: GitAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initRecycler()
+
+    }
+
+    private fun initRecycler() {
         recyclerView = binding.rcvHome
-        val adapter = GitAdapter(requireContext()) { }
-        recyclerView.adapter = adapter
-        adapter.submitList(
-            listOf(
-                GitData(1,"","OB","",0),
-                GitData(2,"", "권용민", "OB",1),
-                GitData(3,"","YB","",0),
-                GitData(4,"", "김준우", "YB",1),
-                GitData(5,"", "김지영", "YB",1),
-                GitData(6,"", "김우남", "YB",1),
-                GitData(7,"", "김준우", "YB",1),
-                GitData(8,"", "김준우", "YB",1),
-                GitData(9,"", "김준우", "YB",1),
-                GitData(10,"", "김준우", "YB",1),
-                GitData(11,"", "김준우", "YB",1),
-                GitData(12,"", "김준우", "YB",1),
-                GitData(13,"", "김준우", "YB",1),
-                GitData(14,"", "김준우", "YB",1),
-                GitData(15,"", "김준우", "YB",1),
-                GitData(16,"", "김준우", "YB",1),
-                GitData(17,"", "김준우", "YB",1),
-                GitData(18,"", "김준우", "YB",1),
-                GitData(19,"", "김준우", "YB",1),
-                GitData(20,"", "김준우", "YB",1),
-            )
+        gitadapter = GitAdapter(requireContext()) { }
+        recyclerView.adapter = gitadapter
+        setSelectionTracker()
+        gitadapter.submitList(gitList.toList())
+    }
+
+    private fun setSelectionTracker() {
+        val tracker = SelectionTracker.Builder<Long>(
+            "gitSelection",
+            recyclerView,
+            StableIdKeyProvider(recyclerView),
+            GitDetailsLookUp(recyclerView),
+            StorageStrategy.createLongStorage()
+        ).withSelectionPredicate(
+            object : SelectionTracker.SelectionPredicate<Long>() {
+                override fun canSetStateForKey(key: Long, nextState: Boolean):
+                        Boolean {
+                    return true
+                }
+
+                override fun canSetStateAtPosition(position: Int, nextState:
+                Boolean): Boolean {
+                    return true
+                }
+
+                override fun canSelectMultiple(): Boolean {
+                    return true
+                }
+            }).build()
+        tracker.addObserver(
+            object : SelectionTracker.SelectionObserver<Long>() {
+                override fun onSelectionChanged() {
+                    super.onSelectionChanged()
+                    val items = tracker.selection.size()
+                    binding.enabled = items >= 1
+                }
+            }
         )
+        gitadapter.setTracker(tracker)
     }
 }
