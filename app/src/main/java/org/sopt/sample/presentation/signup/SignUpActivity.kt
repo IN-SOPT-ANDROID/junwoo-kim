34fdc305567bc02af1bfe5e5498938f5b6d36812
IslandofDream <rhffldkt0517@naver.com>
2022-11-03 12:22:25 +0900

    [Feat] Add LiveData with textChangedListener

--- app/src/main/java/org/sopt/sample/presentation/signup/SignUpActivity.kt
+++ app/src/main/java/org/sopt/sample/presentation/signup/SignUpActivity.kt
@@ -2,6 +2,10 @@ package org.sopt.sample.presentation.signup
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.google.android.material.snackbar.Snackbar
 import org.sopt.sample.R
 import org.sopt.sample.databinding.ActivitySignUpBinding
@@ -11,15 +15,43 @@ import org.sopt.sample.presentation.model.UserData
 
 class SignUpActivity : BindingActivity<ActivitySignUpBinding>(R.layout.activity_sign_up) {
 
+    private var activationId = MutableLiveData<Boolean>(false)
+    private var activationPw = MutableLiveData<Boolean>(false)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        init()
+        binding.enabled = false
+        withLiveData()
+        addObserve()
     }
 
-    private fun init() {
+    private fun withLiveData() {
+        binding.apply {
+            etId.addTextChangedListener {
+                activationId.value = etId.text.length in 6..10
+            }
+            etPw.addTextChangedListener {
+                activationPw.value = etPw.text.length in 8..12
+            }
+        }
+    }
+
+    private fun addObserve() {
+        activationId.observe(this, Observer { //주로 DataBinding 갱신을 해주는 것으로 알고 있음
+            binding.enabled = (activationId.value == true && activationPw.value == true)
+        })
+
+        activationPw.observe(this, Observer {
+            binding.enabled = (activationId.value == true && activationPw.value == true)
+        })
+    }
+
+
+    private fun noLiveData() { // 라이브 데이터를 사용하지 않았을때의 동작
         val intent = Intent(this@SignUpActivity, LoginActivity::class.java)
         binding.apply {
+
             btnSignUp.setOnClickListener {
                 if (etId.text.length in 6..10 && etPw.text.length in 8..12) { //회원가입 조건 ID 6~10 / 비밀번호 8~12
                     intent.apply {
@@ -43,4 +75,5 @@ class SignUpActivity : BindingActivity<ActivitySignUpBinding>(R.layout.activity_
             }
         }
     }
+
 }
