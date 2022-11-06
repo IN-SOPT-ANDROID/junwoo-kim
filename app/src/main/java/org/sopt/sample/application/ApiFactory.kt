968e23bdc168e58ffa28ce8dd0a052dfec2f653d
IslandofDream <rhffldkt0517@naver.com>
2022-11-06 20:03:03 +0900

    [Feat] Add ApiFactory for retrofit

---
+++ app/src/main/java/org/sopt/sample/application/ApiFactory.kt
@@ -0,0 +1,18 @@
+package org.sopt.sample.application
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+object ApiFactory {
+    val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl("http://3.39.169.52:3000/")
+            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+            .build()
+    }
+
+    inline fun <reified T> create(): T = retrofit.create<T>(T::class.java)
+
+}
