3d60a3fdc9fc04d26f05194a92a118282e59e4ac
IslandofDream <rhffldkt0517@naver.com>
2022-11-24 02:50:17 +0900

    [Feat] Add RepositoryImpl

---
+++ app/src/main/java/org/sopt/sample/data/repository/AuthRepositoryImpl.kt
@@ -0,0 +1,19 @@
+package org.sopt.sample.data.repository
+
+import org.sopt.sample.application.ApiFactory
+import org.sopt.sample.data.model.dto.RequestLoginDTO
+import org.sopt.sample.data.model.dto.RequestSingUpDTO
+import org.sopt.sample.data.model.dto.ResponseLoginDTO
+import org.sopt.sample.data.model.dto.ResponseSignUpDTO
+import org.sopt.sample.domain.repository.AuthRepository
+import retrofit2.Response
+
+class AuthRepositoryImpl :AuthRepository {
+    override suspend fun postSingup(dto: RequestSingUpDTO): Response<ResponseSignUpDTO> {
+        return ApiFactory.loginService.signup(dto)
+    }
+
+    override suspend fun postLogin(dto: RequestLoginDTO): Response<ResponseLoginDTO> {
+        return ApiFactory.loginService.login(dto)
+    }
+}
