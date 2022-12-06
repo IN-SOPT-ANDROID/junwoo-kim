62ca4ac95529f1755b8e1d8825fbba65ef641492
IslandofDream <rhffldkt0517@naver.com>
2022-12-07 00:38:17 +0900

    [Feat] Add RepositoryModule with bind annotation

--- app/src/main/java/org/sopt/sample/di/RepositoryModule.kt
+++ app/src/main/java/org/sopt/sample/di/RepositoryModule.kt
@@ -1,10 +1,23 @@
 package org.sopt.sample.di
 
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import org.sopt.sample.data.repository.AuthRepositoryImpl
+import org.sopt.sample.data.repository.ReqresRepositoryImpl
+import org.sopt.sample.domain.repository.AuthRepository
+import org.sopt.sample.domain.repository.ReqresRepository
 import javax.inject.Singleton
 
 @Module
-@InstallIn(Singleton::class)
+@InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindToAuthRepository(authRepositoryImpl: AuthRepositoryImpl): AuthRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindToReqresRepository(repositoryImpl: ReqresRepositoryImpl): ReqresRepository
 }
