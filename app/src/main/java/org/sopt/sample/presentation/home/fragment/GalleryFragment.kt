a08dcfb2642241f0bbb8810ff4647d3a9a8747cc
IslandofDream <rhffldkt0517@naver.com>
2022-12-07 13:25:19 +0900

    [Feat] Add PickImage

--- app/src/main/java/org/sopt/sample/presentation/home/fragment/GalleryFragment.kt
+++ app/src/main/java/org/sopt/sample/presentation/home/fragment/GalleryFragment.kt
@@ -2,7 +2,9 @@ package org.sopt.sample.presentation.home.fragment
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.activityViewModels
+import coil.load
 import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.sample.R
 import org.sopt.sample.databinding.FragmentGalleryBinding
@@ -17,9 +19,16 @@ class GalleryFragment : BindingFragment<FragmentGalleryBinding>(R.layout.fragmen
     }
 
     private val homeViewModel: HomeViewModel by activityViewModels()
+    private val launcher = registerForActivityResult(ActivityResultContracts.GetContent()) {
+        binding.ivImg.load(it)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.viewmodel = homeViewModel
+        binding.btnImage.setOnClickListener {
+            launcher.launch("image/*")
+        }
+
     }
 }
