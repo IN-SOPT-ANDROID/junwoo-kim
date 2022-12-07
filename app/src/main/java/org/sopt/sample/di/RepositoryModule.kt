2fe4b800c74d4746d984dda3794cd46cf5025b35
IslandofDream <rhffldkt0517@naver.com>
2022-12-07 13:54:02 +0900

    [Feat] Add Music Repository in RepositoryModule

--- app/src/main/java/org/sopt/sample/di/RepositoryModule.kt
+++ app/src/main/java/org/sopt/sample/di/RepositoryModule.kt
@@ -5,8 +5,10 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import org.sopt.sample.data.repository.AuthRepositoryImpl
+import org.sopt.sample.data.repository.MusicRepositoryImpl
 import org.sopt.sample.data.repository.ReqresRepositoryImpl
 import org.sopt.sample.domain.repository.AuthRepository
+import org.sopt.sample.domain.repository.MusicRepository
 import org.sopt.sample.domain.repository.ReqresRepository
 import javax.inject.Singleton
 
@@ -20,4 +22,8 @@ abstract class RepositoryModule {
     @Binds
     @Singleton
     abstract fun bindToReqresRepository(repositoryImpl: ReqresRepositoryImpl): ReqresRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindToMusicRepository(repositoryImpl: MusicRepositoryImpl): MusicRepository
 }
