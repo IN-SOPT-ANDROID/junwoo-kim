af01aea07f93b175c5301e28333e0b5f35fe83e3
IslandofDream <rhffldkt0517@naver.com>
2022-12-07 16:35:14 +0900

    [Feat] Repository function for post music

--- app/src/main/java/org/sopt/sample/data/repository/MusicRepositoryImpl.kt
+++ app/src/main/java/org/sopt/sample/data/repository/MusicRepositoryImpl.kt
@@ -1,7 +1,10 @@
 package org.sopt.sample.data.repository
 
+import okhttp3.MultipartBody
 import org.sopt.sample.data.datasource.MusicService
+import org.sopt.sample.data.model.dto.RequestPostMusic
 import org.sopt.sample.data.model.dto.ResponseGetMusicDTO
+import org.sopt.sample.data.model.dto.ResponsePostMusicDTO
 import org.sopt.sample.domain.repository.MusicRepository
 import javax.inject.Inject
 
@@ -10,5 +13,12 @@ class MusicRepositoryImpl @Inject constructor(
 ) : MusicRepository {
     override suspend fun getMusicList(): ResponseGetMusicDTO {
         return musicService.getMusicList()
+    }
+
+    override suspend fun postMusic(
+        map: HashMap<String, RequestPostMusic>,
+        image: MultipartBody.Part
+    ): ResponsePostMusicDTO {
+        return musicService.postMusic(map, image)
     }
 }
--- app/src/main/java/org/sopt/sample/domain/repository/MusicRepository.kt
+++ app/src/main/java/org/sopt/sample/domain/repository/MusicRepository.kt
@@ -1,7 +1,14 @@
 package org.sopt.sample.domain.repository
 
+import okhttp3.MultipartBody
+import org.sopt.sample.data.model.dto.RequestPostMusic
 import org.sopt.sample.data.model.dto.ResponseGetMusicDTO
+import org.sopt.sample.data.model.dto.ResponsePostMusicDTO
 
 interface MusicRepository {
     suspend fun getMusicList(): ResponseGetMusicDTO
+    suspend fun postMusic(
+        map: HashMap<String, RequestPostMusic>,
+        image: MultipartBody.Part
+    ): ResponsePostMusicDTO
 }
