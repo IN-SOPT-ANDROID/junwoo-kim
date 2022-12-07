f72e4652edfad1366f6b6f2254d5b6b24cf5b5fd
IslandofDream <rhffldkt0517@naver.com>
2022-12-07 15:25:25 +0900

    [Feat] Apply GallertViewModel in fragment

--- app/src/main/java/org/sopt/sample/presentation/home/fragment/GalleryFragment.kt
+++ app/src/main/java/org/sopt/sample/presentation/home/fragment/GalleryFragment.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import coil.load
 import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.sample.R
 import org.sopt.sample.databinding.FragmentGalleryBinding
 import org.sopt.sample.presentation.base.BindingFragment
-import org.sopt.sample.presentation.home.viewmodel.HomeViewModel
+import org.sopt.sample.presentation.home.viewmodel.GalleryViewModel
+import timber.log.Timber
 
 @AndroidEntryPoint
 class GalleryFragment : BindingFragment<FragmentGalleryBinding>(R.layout.fragment_gallery) {
@@ -19,16 +20,20 @@ class GalleryFragment : BindingFragment<FragmentGalleryBinding>(R.layout.fragmen
         fun newInstance() = GalleryFragment()
     }
 
-    private val homeViewModel: HomeViewModel by activityViewModels()
+    private val galleryViewModel: GalleryViewModel by viewModels()
     private val launcher = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) {
         binding.ivImg.load(it)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.viewmodel = homeViewModel
+        binding.viewmodel = galleryViewModel
+        binding.lifecycleOwner = viewLifecycleOwner
         binding.btnImage.setOnClickListener {
             launcher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
+        galleryViewModel.musicList.observe(viewLifecycleOwner){
+            Timber.e(it.toString())
         }
 
     }
