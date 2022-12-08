62bad577d2cd1c09cc04b9eb011e8b928f37d81c
IslandofDream <rhffldkt0517@naver.com>
2022-12-08 18:05:51 +0900

    [Feat] add SharedPreference Repository

---
+++ app/src/main/java/org/sopt/sample/data/repository/SharedPrefRepositoryImpl.kt
@@ -0,0 +1,23 @@
+package org.sopt.sample.data.repository
+
+import android.annotation.SuppressLint
+import android.content.SharedPreferences
+import org.sopt.sample.data.util.Constant
+import org.sopt.sample.domain.repository.SharedPrefRepository
+import javax.inject.Inject
+
+class SharedPrefRepositoryImpl @Inject constructor(private val sharedPreferences: SharedPreferences) :
+    SharedPrefRepository {
+    override fun checkLogin(): Boolean {
+        return (sharedPreferences.getString(Constant.PREF_ID, null) == null)
+    }
+
+    @SuppressLint("CommitPrefEdits")
+    override fun setLoginInfo(id: String, pw: String, name: String) {
+        sharedPreferences.edit().apply {
+            putString(Constant.PREF_ID, id)
+            putString(Constant.PREF_PW, pw)
+            putString(Constant.PREF_NAME, name)
+        }.apply()
+    }
+}
--- app/src/main/java/org/sopt/sample/di/RepositoryModule.kt
+++ app/src/main/java/org/sopt/sample/di/RepositoryModule.kt
@@ -7,9 +7,11 @@ import dagger.hilt.components.SingletonComponent
 import org.sopt.sample.data.repository.AuthRepositoryImpl
 import org.sopt.sample.data.repository.MusicRepositoryImpl
 import org.sopt.sample.data.repository.ReqresRepositoryImpl
+import org.sopt.sample.data.repository.SharedPrefRepositoryImpl
 import org.sopt.sample.domain.repository.AuthRepository
 import org.sopt.sample.domain.repository.MusicRepository
 import org.sopt.sample.domain.repository.ReqresRepository
+import org.sopt.sample.domain.repository.SharedPrefRepository
 import javax.inject.Singleton
 
 @Module
@@ -21,9 +23,13 @@ abstract class RepositoryModule {
 
     @Binds
     @Singleton
-    abstract fun bindToReqresRepository(repositoryImpl: ReqresRepositoryImpl): ReqresRepository
+    abstract fun bindToReqresRepository(reqresRepositoryImpl: ReqresRepositoryImpl): ReqresRepository
 
     @Binds
     @Singleton
-    abstract fun bindToMusicRepository(repositoryImpl: MusicRepositoryImpl): MusicRepository
+    abstract fun bindToMusicRepository(musicRepositoryImpl: MusicRepositoryImpl): MusicRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindToSharedPrefRepository(sharedPrefRepositoryImpl: SharedPrefRepositoryImpl): SharedPrefRepository
 }
